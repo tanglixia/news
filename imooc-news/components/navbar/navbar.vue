0d30dacef5dde6d024e9e7d4d98d41517946fdbb
Taryn-Tang <tanglixia822511@163.com>
2020-12-12 22:58:20 +0800

    5-3完

--- imooc-news/components/navbar/navbar.vue
+++ imooc-news/components/navbar/navbar.vue
@@ -1,11 +1,17 @@
 <template>
 	<view class="navbar">
 		<view class="navbar-fixed">
-			<view class="navbar-serach">
-				<view class="navbar-serach_icon">
-				</view>
-				<view class="navbar-serach_text">
-					111122
+			<!-- 状态栏 -->
+			
+			<view :style="{height:statusBarHeight+'px'}"></view>
+			<!-- 导航栏内容 -->
+			<view class="navbar-content" :style="{height:navBarHeight+'px',width:windowWidth+'px'}">
+				<view class="navbar-serach">
+					<view class="navbar-serach_icon">
+					</view>
+					<view class="navbar-serach_text">
+						请输入内容
+					</view>
 				</view>
 			</view>
 		</view>
@@ -17,11 +23,27 @@
 	export default {
 		data() {
 			return {
-				
+				statusBarHeight:20,//默认	状态栏的高度
+				navBarHeight:45, //导航栏的默认高度
+				windowWidth:375
 			};
 		},
+		//组件的生命周期 ，相当于页面中的onload
 		created() {
-		
+			//获取手机系统信息
+			const info = uni.getSystemInfoSync()
+			this.statusBarHeight = info.statusBarHeight
+			this.windowWidth = info.windowWidth
+			// console.log(info);
+			
+			//#ifndef H5 || APP-PLUS || MP-ALIPAY
+			//获取胶囊的位置
+			// 导航栏的高度 = (胶囊底部高度-状态栏的高度)+(胶囊顶部高度-状态栏的高度)
+			const menuButtonInfo = uni.getMenuButtonBoundingClientRect()
+			this.navBarHeight = (menuButtonInfo.bottom-this.statusBarHeight)+(menuButtonInfo.top - this.statusBarHeight)
+			this.windowWidth = menuButtonInfo.left
+			console.log('menB',menuButtonInfo);
+			//#endif
 		}
 	}
 </script>
@@ -29,39 +51,44 @@
 <style lang="scss">
 	.navbar {
 		.navbar-fixed {
-			display: flex;
-			align-items: center;
-			justify-content: center;
 			width: 100%;
-			height: 45px;
 			background-color: $mk-base-color;
-			box-sizing: border-box;
-			padding: 0 15PX;
 			position: fixed;
 			top: 0;
 			left: 0;
 			z-index: 99;
-			.navbar-serach {
+
+			.navbar-content {
 				display: flex;
 				align-items: center;
-				padding: 0 10px;
-				height: 30px;
-				width: 100%;
-				border-radius: 30px;
-				background-color: #fff;
+				justify-content: center;
+				padding: 0 15px;
+				height: 45px;
+				box-sizing: border-box;
 
-				.navbar-serach_icon {
-					width: 10px;
-					height: 10px;
-					border: 1px solid red;
-					margin-right: 10px;
-				}
+				.navbar-serach {
+					display: flex;
+					align-items: center;
+					padding: 0 10px;
+					height: 30px;
+					width: 100%;
+					border-radius: 30px;
+					background-color: #fff;
 
-				.navbar-serach_text {
-					font-size: 12px;
-					color: #999
+					.navbar-serach_icon {
+						width: 10px;
+						height: 10px;
+						border: 1px solid red;
+						margin-right: 10px;
+					}
+
+					.navbar-serach_text {
+						font-size: 12px;
+						color: #999
+					}
 				}
 			}
+
 		}
 	}
 </style>
