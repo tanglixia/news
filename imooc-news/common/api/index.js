931577a1b8222759e3e9d4c657af12f8b626ed08
Taryn-Tang <tanglixia822511@163.com>
2020-12-13 16:21:45 +0800

    5-7数据请求第三种

--- imooc-news/common/api/index.js
+++ imooc-news/common/api/index.js
@@ -1,5 +1,24 @@
-import {get_label} from './list.js'
+// import {get_label} from './list.js'
 
-export default {
-	get_label
-}
+// export default {
+// 	get_label
+// }
+
+
+//批量导出文件
+const requireAPI = require.context(
+	//api 目录的相对路径
+	'.',
+	// 是否查询子目录
+	false,
+	// 查询文件的一个后缀
+	/.js$/
+)
+
+let module = {}
+console.log(requireAPI.keys());
+requireAPI.keys().forEach((key,index)=>{
+	if(key === './index.js') return 
+	Object.assign(module,requireAPI(key))
+})
+export default module
